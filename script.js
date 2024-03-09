a309e2f5b12772c5f25e1ecca295fb81e1ce2a9e
tanmoyux <78644667+tanmoyux@users.noreply.github.com>
2024-03-09 15:27:06 +0100

    Update sytles and javascript

--- script.js
+++ script.js
@@ -20,27 +20,6 @@ function CopyToClipboard(id)
 			})
 		}
 
-
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
-	  var dropdowns = document.getElementsByClassName("dropdown-content");
-	  var i;
-	  for (i = 0; i < dropdowns.length; i++) {
-		var openDropdown = dropdowns[i];
-		if (openDropdown.classList.contains('show')) {
-		  openDropdown.classList.remove('show');
-		}
-	  }
-	}
-  }
-
  /*MOBILE MESSAGE*/ 
   setTimeout(() => {
 	const disappear = document.getElementById('disappear');
